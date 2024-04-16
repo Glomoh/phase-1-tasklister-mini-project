48ded2ffefaf5528b809862dd8de9cc821e1087d
Glomoh <glowshakina@gmail.com>
2024-04-16 07:49:02 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('create-task-form', (e)=> {
+    e.preventdefault () 
+    buildtodo(e.target.new_todo.value) 
+     })
 });
+
+function buildtodo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement(button)
+  btn.addEventListener('click', handleDelete)
+  p.textContent = 'x'
+  p.textContent = `${todo}`
+  console.log(p)
+document.querySelector('#create-task-form').{appendChild(p)}
+
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
